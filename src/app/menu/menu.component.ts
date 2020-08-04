4ca14eb86bef041115e1da8cf311163e6adae984
Ali Raza <alirazaalisher345@gmail.com>
2020-08-04 14:38:04 +0500

    Animations Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, Inject, Injectable } from "@angular/core";
 import { Dish } from "../shared/dish";
 import { DishService } from "../services/dish.service";
+import { flyInOut, expand } from "../animations/app.animation";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    "[@flyInOut]": "true",
+    style: "display: block;",
+  },
+  animations: [flyInOut(), expand()],
 })
 @Injectable({
   providedIn: "root",
